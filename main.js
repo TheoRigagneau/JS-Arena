add62d19b8265403c1e3f568f4685b4ab8544d5b
TheoRigagneau <trigagneau@gmail.com>
2026-03-04 22:15:18 +0100

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-import Game from "./model/Game.js";
-import GameView from "./view/GameViews.js";
-import GameController from "./controller/GameController.js";
-
-const gameController = new GameController(Game,GameView);
